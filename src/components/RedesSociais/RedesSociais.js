ff9c2853cf23953896fa1621796a075cff15f907
Guilherme Ferreira <guilhermefabricio564@gmail.com>
2025-11-09 13:10:00 -0300

    Adiciona estilos de App e Header

--- src/components/RedesSociais/RedesSociais.js
+++ src/components/RedesSociais/RedesSociais.js
@@ -2,13 +2,9 @@ import './RedesSociais.css';
 
 const RedesSociais = () => {
   const ListaRedes = [
-    { id: 1, urlNome: 'Banner', url: 'https://www.banner.com' },
-    { id: 2, urlNome: 'Instagram', url: 'https://www.instagram.com' },
-    { id: 3, urlNome: 'Documentação', url: 'https://www.banner.com' },
-    { id: 4, urlNome: 'Vídeo Pitch', url: 'https://www.banner.com' },
-    { id: 5, urlNome: 'Artigo', url: 'https://www.banner.com' },
-    { id: 6, urlNome: 'Resultados', url: 'https://www.banner.com' },
-    { id: 7, urlNome: 'LinkedIn do Guilherme', url: 'https://www.linkedin.com/in/guilhermeff45' }
+    { id: 1, urlNome: 'Vídeo Pitch', url: 'https://youtube.com/shorts/RGIExyxHXPQ' },
+    { id: 2, urlNome: 'Trello', url: 'https://trello.com/b/GyndirEf/upx-3' },
+    { id: 3, urlNome: 'GitHub', url: 'https://github.com/LinkEdu-AI/LinkEdu' }
   ];
 
   return (
